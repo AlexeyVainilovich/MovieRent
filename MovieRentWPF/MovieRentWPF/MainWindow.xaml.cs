9c807b2717847e1da15c7d7b4260642da4022391
AlexeyVainilovich <Alexey.Vainilovich@mail.ru>
2016-12-10 01:51:43 +0300

    Add files via upload

---
+++ MovieRentWPF/MovieRentWPF/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MovieRentWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            SettingsWindow settingWindow = new SettingsWindow();
+            settingWindow.Show();
+        }
+
+        private void ActorsButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Hide();
+            ActorsListWindow actorsListWindow = new ActorsListWindow();
+            actorsListWindow.Show();
+        }
+
+        private void MoviesButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Hide();
+            MoviesListWindow moviesListWindow = new MoviesListWindow();
+            moviesListWindow.Show();
+        }
+
+        private void ProducersButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Hide();
+            ProducersListWindow producersListWindow = new ProducersListWindow();
+            producersListWindow.Show();
+        }
+
+        private void FindButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Hide();
+            FindWindow findWindow = new FindWindow();
+            findWindow.Show();
+        }
+    }
+}
---
+++ MovieRentWPF/MovieRentWPF/SettingsWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MovieRentWPF
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            var mainWindow = (MainWindow)(Application.Current.MainWindow);
+            mainWindow.Show();
+        }
+
+        private void actorAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ActorAddWindow actorAddWindow = new ActorAddWindow();
+            actorAddWindow.Show();
+        }
+
+        private void movieAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            MovieAddWindow movieAddWindow = new MovieAddWindow();
+            movieAddWindow.Show(); 
+        }
+
+        private void produserAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ProducerAddWindow producerAddWindow = new ProducerAddWindow();
+            producerAddWindow.Show();
+        }
+    }
+}
